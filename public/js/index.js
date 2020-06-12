c46ff9fa3f83293cd5ec74cfce475c40662e41ad
Mateus Queirós <45767743+mateuscqueiros@users.noreply.github.com>
2020-06-12 11:39:22 -0300

    Versao final

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,11 @@
 const body = document.querySelector('body');
 const btnOpen = document.getElementById('menu-open');
+const btnClose = document.getElementById('close-menu');
 
 btnOpen.addEventListener("click", () => {
     body.classList.add("open-menu");
+})
+
+btnClose.addEventListener("click", () => {
+    body.classList.remove("open-menu");
 })
